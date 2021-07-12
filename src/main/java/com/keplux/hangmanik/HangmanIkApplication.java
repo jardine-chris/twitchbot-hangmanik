8284af008e0a8b45d5b70e95bd47b4e5b49cd3f9
Chris Jardine <c_jardine@icloud.com>
2021-07-11 21:49:51 -0400

    Initial commit.

---
+++ src/main/java/com/keplux/hangmanik/HangmanIkApplication.java
@@ -0,0 +1,18 @@
+package com.keplux.hangmanik;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class HangmanIkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangmanIkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keplux/hangmanik/HangmanIkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keplux.hangmanik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanIkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
